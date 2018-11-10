f6f247aaf7ffb703595023d7538003a68c3328c8
Mehret Kulala <merutaye@gmail.com>
2018-11-10 11:42:12 -0600

    created model

---
+++ Bangazon/Models/EmployeeTrainingProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models
+{
+    public class EmployeeTrainingProgram
+    {
+        public int Id { get; set; }
+    }
+}
