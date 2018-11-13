2cea2ec565596572e752507c4e3c5556306fcc40
brian <briankw23@gmail.com>
2018-11-12 20:24:54 -0600

    getting all orders

--- Bangazon/DataAccess/OrdersStorage.cs
+++ Bangazon/DataAccess/OrdersStorage.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bangazon.Models;
+using Dapper;
+using System.Data.SqlClient;
 
 namespace Bangazon.DataAccess
 {
@@ -15,6 +18,19 @@ namespace Bangazon.DataAccess
         }
 
         // API functions go here, use ConnectionString for new SqlConnection
+
+        public List<Orders> GetAllOrders()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                List<Orders> Orders = new List<Orders>();
+
+                var result = connection.Query<Orders>(@"select * from Orders");
+                return result.ToList();
+            }
+        }
 
     }
 }
