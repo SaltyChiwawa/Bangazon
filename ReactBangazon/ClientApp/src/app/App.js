0dbb33dfb5b5f7427bda553c7724b2c8f5c8fba8
Drake Morrison <drakethedragonprogrammer@gmail.com>
2018-11-27 14:40:42 -0600

    file structure changes

--- ReactBangazon/ClientApp/src/app/App.js
+++ ReactBangazon/ClientApp/src/app/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { Layout } from '../components/Layout/Layout';
+import { Home } from '../components/Home/Home';
+import { FetchData } from '../components/FetchData/FetchData';
+import { Counter } from '../components/Counter/Counter';
 
 export default class App extends Component {
   displayName = App.name
