a112d1902166dc96f657a7bf7da0032e1bc2f84a
Drake Morrison <drakethedragonprogrammer@gmail.com>
2018-11-27 19:12:31 -0600

    react base structure in js files

--- ReactBangazon/ClientApp/src/components/BackButton/BackButton.js
+++ ReactBangazon/ClientApp/src/components/BackButton/BackButton.js
@@ -1,1 +1,14 @@
-﻿
+﻿import React from 'react';
+import { Link } from 'react-router-dom';
+
+class BackButton extends React.Component {
+    render() {
+        return (
+            <div className='BackButton'>
+                <p><Link to='/' className='btn btn-lg btn-success'>Back to Home</Link></p>
+            </div>
+        );
+    };
+}
+
+export default BackButton;
