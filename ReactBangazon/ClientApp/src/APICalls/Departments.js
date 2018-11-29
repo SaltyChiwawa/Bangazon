8cc045100bdfa47fb228a3b5d2cec3320d52993a
Drake Morrison <drakethedragonprogrammer@gmail.com>
2018-11-28 20:03:25 -0600

    react and api talking

--- ReactBangazon/ClientApp/src/APICalls/Departments.js
+++ ReactBangazon/ClientApp/src/APICalls/Departments.js
@@ -1,20 +1,11 @@
-
 import axios from 'axios';
-import constants from '../constants';
 
 const getRequest = () => {
   return new Promise((resolve, reject) => {
     axios
-      .get(`localhost:3000/api/customers/customers`)
+        .get(`https://localhost:44372/api/departments`)
       .then(res => {
-        const stuff = [];
-        if (res.data !== null) {
-          Object.keys(res.data).forEach(fbKey => {
-            res.data[fbKey].id = fbKey;
-            stuff.push(res.data[fbKey]);
-          });
-        }
-        resolve(stuff);
+        resolve(res.data);
       })
       .catch(err => {
         reject(err);
