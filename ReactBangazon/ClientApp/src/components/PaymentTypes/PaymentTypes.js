c29b8ed37a0e379597c0988bb798ae6d6cd6af52
Mehret Kulala <merutaye@gmail.com>
2018-12-01 09:54:06 -0600

    getting paymentTypes wip

--- ReactBangazon/ClientApp/src/components/PaymentTypes/PaymentTypes.js
+++ ReactBangazon/ClientApp/src/components/PaymentTypes/PaymentTypes.js
@@ -1,1 +1,45 @@
-﻿
+﻿import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import getPaymentTypesRequest from '../../DbRequests/PaymentType';
+//mport PaymentTypesLineItem from './PaymentTypesLineItem';
+
+class PaymentTypes extends Component {
+    state = { paymentTypes: [] }
+
+    getAllPaymnetTypes = () => {
+        getPaymentTypesRequest.getAllPaymentTypes()
+            .then((paymentTypes) => {
+                this.setState({ paymentTypes });
+            })
+            .catch((err) => {
+                console.error('Error getting PaymentTypes: ', err);
+            });
+    }
+
+    paymentLintItem = () => {
+        const paymnetTypes = this.props.paymnetTypes;
+        return paymnetTypes.map((paymnetType) => {
+            return (
+                <tr key={paymnetType.id}>
+                    <td>{paymnetType.name}</td>
+                </tr>
+            );
+        });
+    };
+
+
+    //updateState = () => {
+    //    this.getAllPaymentTypes();
+    //};
+
+    render() {
+        return (
+            <div className='PaymentTypes'>
+                <p><Link to='/' className='btn btn-lg btn-success'>Back to Home</Link></p>
+                <button type="button" class="btn btn-primary" onClick={this.getAllPaymnetTypes}>PaymentTypes</button>
+            </div>
+        );
+    };
+}
+
+export default PaymentTypes;
