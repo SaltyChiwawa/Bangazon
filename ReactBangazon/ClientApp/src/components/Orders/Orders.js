31b166fa496446910cfec8d4068613d988567258
brian <briankw23@gmail.com>
2018-12-01 08:13:48 -0600

    wip

--- ReactBangazon/ClientApp/src/components/Orders/Orders.js
+++ ReactBangazon/ClientApp/src/components/Orders/Orders.js
@@ -1,6 +1,7 @@
 ﻿import React from 'react';
 import { Link } from 'react-router-dom';
 import OrdersRequest from '../../APICalls/Orders';
+import react
 
 class Orders extends React.Component {
     state = {
@@ -12,16 +13,32 @@ class Orders extends React.Component {
             .then(orders => {
                 this.setState({ orders });
                 console.log(orders);
+                console.log("state:", this.state.orders);
             })
             .catch(err => {
                 console.error(err, 'error getting order');
             });
     }
-    //getRequest
-
     render() {
-        //const allOrders = 
-        return(<h1>Orders</h1>)
+        const allOrders = this.state.orders.map(orda => {
+            return (
+            <div>
+            <div>
+                <div class="row">
+                    <div class="col-md-8">.col-md-8</div>
+                    <div class="col-md-4">.col-md-4</div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">.col-md-4</div>
+                    <div class="col-md-4">.col-md-4</div>
+                    <div class="col-md-4">.col-md-4</div>
+                </div>
+            </div>
+            </div>
+                )
+        })
+
+        return ({ allOrders });
     }
 }
 
