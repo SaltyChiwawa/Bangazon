e8122ddb86297070994fbfa4aa648371e1fc728f
Maken Hubbard <makhub26@outlook.com>
2018-12-01 09:28:53 -0600

    wip

--- ReactBangazon/ClientApp/src/components/Computers/Computers.js
+++ ReactBangazon/ClientApp/src/components/Computers/Computers.js
@@ -1,14 +1,62 @@
 ﻿import React from 'react';
 import { Link } from 'react-router-dom';
+import computersRequests from '../../APICalls/ComputersRequests';
 
 class Computers extends React.Component {
-    render() {
+    state = {
+        computers : [],
+    };
+
+    getComputersRequest = (e) => {
+        computersRequests
+            .getAllComputersRequest()
+                .then((comps) => {
+                    this.setState({ computers : comps})
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        };
+
+
+render() {
+
+    const compData = this.state.computers.map(comps => {
         return (
-            <div className='Computers'>
+            <div key={comps.Id} className="panel panel-default" >
+                <div className="panel-heading">
+                    <h3 className="panel-title">{comps.Id}</h3>
+                </div>
+                <div className="panel-body">
+                    <p>Employee Id {comps.EmployeeId}</p>
+                </div>
+            </div >
+        );
+    });
+
+    return (
+        <div className='Computers'>
+            <div>
                 <p><Link to='/' className='btn btn-lg btn-success'>Back to Home</Link></p>
             </div>
-        );
-    };
-}
+            <div className="form-group">
+                <div className="col-md-offset-3 col-sm-10">
+                    <button type="submit" className="getAllComputers btn btn-md btn-warning" onClick={this.getComputersRequest}>See All Computers</button>
+                    <div>
+                        {compData}
+                    </div>
+                    <button type="submit" className="col-md-offset-2 btn btn-md btn-warning notASingleComputer"> Nope no Single Computers</button>
+                </div>
+                <div className="col-md-offset-3">
+                    <p> some content here </p>
+                    <button type="submit" className="col-sm-2 btn btn-md btn-primary" id="editComputerButt"> Edit </button>
+                    <button type="submit" className="col-md-offset-3 col-sm-2 btn btn-md btn-danger" id="deleteComputerButt"> Delete </button>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+};
 
 export default Computers;
