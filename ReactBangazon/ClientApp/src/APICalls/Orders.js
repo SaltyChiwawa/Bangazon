34f3249b2baff4627cecdc6a123751827a26c989
brian <briankw23@gmail.com>
2018-12-01 07:26:53 -0600

    Orders are consoling

---
+++ ReactBangazon/ClientApp/src/APICalls/Orders.js
@@ -0,0 +1,16 @@
+﻿import axios from 'axios';
+
+const getRequest = () => {
+    return new Promise((resolve, reject) => {
+        axios
+            .get(`api/orders`)
+            .then(res => {
+                resolve(res.data);
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
+};
+
+export default { getRequest };
--- ReactBangazon/ClientApp/src/components/Orders/Orders.js
+++ ReactBangazon/ClientApp/src/components/Orders/Orders.js
@@ -1,1 +1,28 @@
-﻿
+﻿import React from 'react';
+import { Link } from 'react-router-dom';
+import OrdersRequest from '../../APICalls/Orders';
+
+class Orders extends React.Component {
+    state = {
+        orders: [],
+    }
+    componentDidMount() {
+        OrdersRequest
+            .getRequest()
+            .then(orders => {
+                this.setState({ orders });
+                console.log(orders);
+            })
+            .catch(err => {
+                console.error(err, 'error getting order');
+            });
+    }
+    //getRequest
+
+    render() {
+        //const allOrders = 
+        return(<h1>Orders</h1>)
+    }
+}
+
+export default Orders;
