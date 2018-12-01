a1efa2703b26de48be8a062b116690908947fb8e
Drake Morrison <drakethedragonprogrammer@gmail.com>
2018-12-01 09:50:45 -0600

    getAll working

--- ReactBangazon/ClientApp/src/components/Departments/Departments.js
+++ ReactBangazon/ClientApp/src/components/Departments/Departments.js
@@ -7,6 +7,7 @@ class Departments extends React.Component {
         departments: [],
     }
 
+    // Set state for departments
     getDepartments = (e) => {
         departmentRequests
             .getRequest()
@@ -16,12 +17,23 @@ class Departments extends React.Component {
             .catch(console.error.bind(console));
     };
 
+    deleteDepartment = (e) => {
+        console.error(e.target);
+        departmentRequests
+            .deleteRequest()
+            .then(() => {
+                this.getDepartments();
+            })
+            .catch(console.error.bind(console));
+    }
+
     render() {
         // Make DOM nodes for departments data
         const dptElements = this.state.departments.map(dpt => {
             return (
                 <div key={dpt.id} className='well well-sm'>
                     <h4>{dpt.name}</h4>
+                    <button className='btn btn-error' onClick={this.deleteDepartment}>Delete</button>
                 </div>
             );
         });
@@ -29,8 +41,8 @@ class Departments extends React.Component {
         return (
             <div className='Departments'>
                 <p><Link to='/' className='btn btn-lg btn-success btn-block'>Back to Home</Link></p>
-                <p className='btn btn-lg btn-block' onClick={this.getDepartments}>Get All Departments</p>
-                <div>
+                <p className='btn btn-lg btn-primary btn-block' onClick={this.getDepartments}>Get All Departments</p>
+                <div className='col-sm-12 department-page'>
                     {dptElements}
                 </div>
             </div>
