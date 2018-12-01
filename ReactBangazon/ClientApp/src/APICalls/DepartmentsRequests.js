43b872bc085ddb327a7a921f24611adf0c65fb17
Drake Morrison <drakethedragonprogrammer@gmail.com>
2018-12-01 11:24:37 -0600

    wip

--- ReactBangazon/ClientApp/src/APICalls/DepartmentsRequests.js
+++ ReactBangazon/ClientApp/src/APICalls/DepartmentsRequests.js
@@ -13,12 +13,4 @@ const getRequest = () => {
   });
 };
 
-const deleteRequest = () => {
-    return new Promise((resolve, reject) => {
-        axios
-            .get('https://localhost:44372/api/departments')
-            .then()
-    })
-};
-
-export default { getRequest, deleteRequest };
+export default { getRequest };
--- ReactBangazon/ClientApp/src/components/Departments/Departments.js
+++ ReactBangazon/ClientApp/src/components/Departments/Departments.js
@@ -1,10 +1,12 @@
 ﻿import React from 'react';
 import { Link } from 'react-router-dom';
-import departmentRequests from '../../APICalls/Departments';
+import departmentRequests from '../../APICalls/DepartmentsRequests';
 
 class Departments extends React.Component {
     state = {
         departments: [],
+        name: '',
+        supervisorId: 0,
     }
 
     // Set state for departments
@@ -17,23 +19,13 @@ class Departments extends React.Component {
             .catch(console.error.bind(console));
     };
 
-    deleteDepartment = (e) => {
-        console.error(e.target);
-        departmentRequests
-            .deleteRequest()
-            .then(() => {
-                this.getDepartments();
-            })
-            .catch(console.error.bind(console));
-    }
-
     render() {
         // Make DOM nodes for departments data
         const dptElements = this.state.departments.map(dpt => {
             return (
                 <div key={dpt.id} className='well well-sm'>
                     <h4>{dpt.name}</h4>
-                    <button className='btn btn-error' onClick={this.deleteDepartment}>Delete</button>
+                    <h5>SupervisorId: {dpt.supervisorId}</h5>
                 </div>
             );
         });
@@ -41,10 +33,33 @@ class Departments extends React.Component {
         return (
             <div className='Departments'>
                 <p><Link to='/' className='btn btn-lg btn-success btn-block'>Back to Home</Link></p>
+
                 <p className='btn btn-lg btn-primary btn-block' onClick={this.getDepartments}>Get All Departments</p>
+
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <span>Name:</span>
+                        <input
+                            type="text"
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <label>
+                        <span>SupervisorId</span>
+                        <input
+                            type="number"
+                            value={this.state.supervisorId}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <input type="submit" value="Post Department" />
+                </form>
+
                 <div className='col-sm-12 department-page'>
                     {dptElements}
                 </div>
+
             </div>
         );
     };
