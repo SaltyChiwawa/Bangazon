ea2b90c3115069f806794d0b520de5982ecf1bdf
Maken Hubbard <makhub26@outlook.com>
2018-12-03 19:32:24 -0600

    adding changes to the the get all computers and added add button for the add computer function WIP

--- ReactBangazon/ClientApp/src/components/Computers/Computers.js
+++ ReactBangazon/ClientApp/src/components/Computers/Computers.js
@@ -7,7 +7,7 @@ class Computers extends React.Component {
         computers: [],
     };
 
-    getComputersRequest = (e) => {
+    componentDidMount = (e) => {
         computersRequests
             .getAllComputersRequest()
             .then((comps) => {
@@ -21,34 +21,32 @@ class Computers extends React.Component {
 
     render() {
 
-    const compData = this.state.computers.map(comps => {
-        return (
-            <div key={comps.id} className="panel panel-default" >
-                <div className="panel-heading">
-                    <h3 className="panel-title">{comps.id}</h3>
-                </div>
-                <div className="panel-body">
-                    <p>Employee Id {comps.employeeId}</p>
-                    <div className="col-md-offset-3">
-                        <button type="submit" className="col-sm-2 btn btn-md btn-primary" id="editComputerButt"> Edit </button>
-                        <button type="submit" className="col-md-offset-3 col-sm-2 btn btn-md btn-danger" id="deleteComputerButt"> Delete </button>
+        const compData = this.state.computers.map(comps => {
+            return (
+                <div key={comps.id} className="panel panel-default" >
+                    <div className="panel-heading">
+                        <h3 className="panel-title">{comps.id}</h3>
                     </div>
-                </div>
-            </div >
-        );
-    });
+                    <div className="panel-body">
+                        <p>Employee Id {comps.employeeId}</p>
+                        <div className="col-md-offset-3">
+                            <button type="submit" className="col-sm-2 btn btn-md btn-primary" id="editComputerButt"> Edit </button>
+                            <button type="submit" className="col-md-offset-3 col-sm-2 btn btn-md btn-danger" id="deleteComputerButt"> Delete </button>
+                        </div>
+                    </div>
+                </div >
+            );
+        });
 
         return (
             <div className='Computers'>
                 <div>
                     <p><Link to='/' className='btn btn-lg btn-success'>Back to Home</Link></p>
                 </div>
-                <div className="form-group">
-                    <div className="col-md-offset-3 col-sm-10">
-                        <button type="submit" className="getAllComputers btn btn-md btn-warning" onClick={this.getComputersRequest}>See All Computers</button>
-                        <div>
-                            {compData}
-                        </div>
+                <div>
+                    {compData}
+                    <div>
+                        <button type="button" class="btn btn-info"><span className="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
                     </div>
                 </div>
             </div>
