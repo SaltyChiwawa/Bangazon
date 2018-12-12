3deb4b1bafa8950fd0324a2e304fd3a8c4a44fc5
Maken Hubbard <makhub26@outlook.com>
2018-12-11 19:07:54 -0600

    I have the names for the computers assigned to which employee

--- ReactBangazon/ClientApp/src/components/Computers/Computers.js
+++ ReactBangazon/ClientApp/src/components/Computers/Computers.js
@@ -22,6 +22,7 @@ class Computers extends React.Component {
             .getAllComputersRequest()
             .then((comps) => {
                 this.setState({ computers: comps });
+                console.log(comps);
             })
             .catch((err) => {
                 console.error(err);
@@ -85,10 +86,11 @@ class Computers extends React.Component {
         const compData = this.state.computers.map(comps => {
             return (
                 <div key={comps.id} className="panel panel-default" >
-                    <div className="panel-heading">                        <h3 className="panel-title">{comps.id}</h3>
+                    <div className="panel-heading">                        <h3 className="panel-title">Computers Id: {comps.id}</h3>
                     </div>
                     <div className="panel-body">
-                        <p>Employee Id {comps.employeeId}</p>
+                        <p>Employee: {`${comps.firstName}` + " " + `${comps.lastName}`}</p>
+                        <p>Employee Id: {comps.employeeId}</p>
                         <div className="col-md-offset-3">
                             <button type="submit" className="col-sm-2 btn btn-md btn-primary" id="editComputerButt"> Edit </button>
                             <button type="submit" className="col-md-offset-3 col-sm-2 btn btn-md btn-danger" id="deleteComputerButt" onClick={this.deleteComputer} data-id={comps.id}> Delete </button>
