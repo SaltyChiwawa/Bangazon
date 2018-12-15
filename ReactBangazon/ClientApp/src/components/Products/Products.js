b8e9bffa3b269a307887785c5f0b6b17ab0c8901
Maken Hubbard <makhub26@outlook.com>
2018-12-15 14:05:01 -0600

    adding basic modal

--- ReactBangazon/ClientApp/src/components/Products/Products.js
+++ ReactBangazon/ClientApp/src/components/Products/Products.js
@@ -1,13 +1,19 @@
 ﻿import React from 'react';
 import { Link } from 'react-router-dom';
+import { Modal, Button } from 'react-bootstrap';
 import productsRequests from '../../APICalls/ProductsRequests';
 
 class Products extends React.Component {
     state = {
         products: [],
+        isClicked: false,
     };
 
-    getProductsRequest = (e) => {
+    componentDidMount = (e) => {
+        this.getAllProducts();
+    } 
+
+    getAllProducts = () => {
         productsRequests
             .getAllProductsRequest()
             .then((prod) => {
@@ -17,6 +23,16 @@ class Products extends React.Component {
                 console.error('error in getProductsRequest', err);
             });
     };
+
+    //----------------------------------Modal Handlers---------------------//
+
+    addProductModal = (e) => {
+        this.setState({ isClicked: true });
+    }
+
+    closeModal = (e) => {
+        this.setState({ isClicked: false });
+    }
 
 
     render() {
@@ -48,14 +64,84 @@ class Products extends React.Component {
                     <p><Link to='/' className='btn btn-lg btn-success'>Back to Home</Link></p>
                 </div>
                 <div className="form-group">
-                    <div className="col-md-offset-3 col-sm-10">
-                        <button type="submit" className="getAllProducts btn btn-md btn-warning" onClick={this.getProductsRequest}>See All Products</button>
-                        <div>
-                            {productData}
-                        </div>
-                    </div>
+                    {productData}
                 </div>
-            </div>
+
+                <Modal show={this.state.isClicked} onHide={this.closeModal}>
+                    <Modal.Header>
+                        <Modal.Title>Add a New Product</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <form className="form-inline-block">
+                            <div className="form-group">
+                                <label htmlFor="exampleInputName2">Product Type Id </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="addProdTypeId"
+                                    placeholder="ex. 1"
+                                    //value={}
+                                    //onChange={}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputName2">Title </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="addProdTitle"
+                                    placeholder="ex. Dishwasher Necklace"
+                                    //value={}
+                                    //onChange={}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputName2">Description </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="addProdDesc"
+                                    placeholder="ex. Wash dishes on the go, WEARever you go"
+                                //value={}
+                                //onChange={}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputName2">Quantity </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="addProdQuantity"
+                                    placeholder="ex. 5"
+                                //value={}
+                                //onChange={}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputName2">Customer Id </label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="addCustomerId"
+                                    placeholder="ex. 5"
+                                //value={}
+                                //onChange={}
+                                />
+                            </div>
+                        </form>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button onClick={this.closeModal}>Close</Button>
+
+                        <Button bsStyle="primary" onClick={this.addProduct}>Save changes</Button>
+
+                    </Modal.Footer>
+                </Modal>
+
+                <button type="button" className="btn btn-info" onClick={this.addProductModal}><span className="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
+            </div>            
         );
     };
 };
