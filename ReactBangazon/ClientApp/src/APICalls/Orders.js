56d5bc0d2ac224bf9cbe9c4b5bf60b6f110a653a
Mehret Kulala <merutaye@gmail.com>
2018-12-18 18:15:07 -0600

    orders update wip

--- ReactBangazon/ClientApp/src/APICalls/Orders.js
+++ ReactBangazon/ClientApp/src/APICalls/Orders.js
@@ -30,4 +30,30 @@ const deleteRequest = (id) =>
     })
 };
 
-export default { getRequest, deleteRequest };
+const updateRequest = (id) => {
+    return new Promise((resolve, reject) => {
+        axios
+            .put(`api/orders/${id}`)
+            .then(res => {
+                resolve(res.data);
+            })
+            .catch((err)=> {
+                reject(err);
+            })
+    })
+}
+
+
+const updatePaymentType = (updatePaymentType) => {
+    return new Promise((resolve, reject) => {
+        axios
+            .put(`api/paymentTypes/paymentType`, updatePaymentType)
+            .then(res => {
+                resolve(res.data);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
+};
+export default { getRequest, deleteRequest, updateRequest };
--- ReactBangazon/ClientApp/src/components/Orders/Orders.js
+++ ReactBangazon/ClientApp/src/components/Orders/Orders.js
@@ -1,11 +1,17 @@
 ﻿import React from 'react';
 import { Link } from 'react-router-dom';
 import OrdersRequest from '../../APICalls/Orders';
+import { Modal, Button } from 'react-bootstrap';
 
 class Orders extends React.Component {
     state = {
         orders: [],
+        editId: '',
+        editCustomerId: '',
+        editProductId: '',
+        showEdit: false
     }
+
     getallRequest = () => {
         OrdersRequest
             .getRequest()
@@ -23,7 +29,6 @@ class Orders extends React.Component {
         this.getallRequest();
     }
 
-    //Add delete request
 
     deleteAnOrder = (id) =>
     {
@@ -38,6 +43,36 @@ class Orders extends React.Component {
                 console.error("error with delete order", err)
             })
     }
+    // show Modal
+    showEditModal = (e) => {
+        const orderId = e.target.dataset.id;
+        const customerId = e.target.dataset.customerId;
+        const productId = e.target.dataset.productId;
+        this.setState({ showEdit: true, editId: orderId, editCustomerId: customerId, editProductId: productId });
+    }
+
+    // close Modal
+    closeEditModal = () => {
+        this.setState({ showEdit: false, editId: '', editCustomerId: '', editProductId: '' });
+    }
+
+    // IdChange
+    idChange = (e) => {
+        this.setState({editId: e.traget.value})
+    }
+
+    //CustomerIdChange
+    customerIdChange = (e) => {
+        this.setState({editCustomerId: e.target.value})
+    }
+
+    //PaymentIdChange
+    paymentIdChange = (e) => {
+        this.setState({ editProductId: e.target.value})
+    }
+
+
+
     render()
     {
         const allOrders = this.state.orders.map(orda => {
@@ -56,7 +91,7 @@ class Orders extends React.Component {
                         </div>
                         <div>
                             <button className="btn btn-default" onClick={() => this.deleteAnOrder(orda.id)}>Delete</button>
-                            <button className="btn btn-default">Update</button>                            
+                            <button className="btn btn-default" data-id={orda.id} data-customerId={orda.customerId} data-productId={orda.productId}onClick={this.showEditModal}>Update</button>
                         </div>
                     </div>
 
@@ -96,7 +131,34 @@ class Orders extends React.Component {
                         <button type="button" className="col-md-12 btn btn-default">Create New Order</button>
                 </div>
                 </div>
-                {allOrders}
+                <span>
+                    {allOrders}
+                </span>
+
+                <Modal show={this.state.showEdit} onHide={this.closeEditModal}>
+                    <Modal.Header>
+                        <Modal.Title>Update Order</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <input placeholder="Id"
+                            value={this.state.editId}
+                            onChange={this.idChange} />
+                        {' '}
+                        <input placeholder="orderId"
+                            value={this.state.editCustomerId}
+                            onChange={this.customerIdChange} />
+                        {' '}
+                        <input placeholder="PaymentId"
+                            value={this.state.editPaymentTypeId}
+                            onChange={this.paymentIdChange} />
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button onClick={this.closeEditModal}>Close</Button>
+                        <Button bsStyle="primary">Save changes</Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         );
     }
