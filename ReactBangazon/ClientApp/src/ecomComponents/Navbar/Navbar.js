7e7dbed69c3996e165a765fec483a03d2389b65b
Anna Dolan <anna.dolan115@gmail.com>
2019-01-16 18:46:48 -0600

    added basic navbar

--- ReactBangazon/ClientApp/src/ecomComponents/Navbar/Navbar.js
+++ ReactBangazon/ClientApp/src/ecomComponents/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 ﻿import React from 'react';
 import logo from '../../Images/bangazon_logo.png';
+import {Link} from 'react-router-dom';
 import './Navbar.css';
 import { Navbar, Button, FormGroup, FormControl} from 'react-bootstrap';
 
@@ -20,7 +21,29 @@ class Nav extends React.Component {
                             <a class="navbar-brand" href="#home"><img src={logo} responsive id="nav-logo" alt="Bangazon Logo" /></a>
                         </Navbar.Brand>
                     </Navbar.Header>
-                    <Navbar.Form id="nav-form">
+                        <form class="form-horizontal" id="nav-search">
+                            <div class="form-group">
+                                <div class="col-sm-5 col-sm-offset-1">
+                                    <input 
+                                        type="text" 
+                                        class="form-control" 
+                                        placeholder="Search"
+                                        value={this.state.value}
+                                        onChange={this.handleChange}
+                                    ></input>
+                                </div>
+                                <button type="button" class="btn btn-warning" id="nav-search-btn">
+                                    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                                </button>
+                                <Link to='/Login' id="nav-login-link">Login</Link>
+                                <span 
+                                    id="nav-cart-logo"
+                                    class="glyphicon glyphicon-shopping-cart pull-right" 
+                                    aria-hidden="true"
+                                >Cart</span>
+                            </div>
+                        </form>
+                    {/* <Navbar.Form>
                         <FormGroup>
                             <FormControl
                                 type="text"
@@ -32,8 +55,16 @@ class Nav extends React.Component {
                         <Button
                             type="submit"
                         >Submit</Button>
-                    </Navbar.Form>
+                        <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true">Cart</span>
+                    </Navbar.Form> */}
                 </Navbar>
+                {/* <form class="form-horizontal">
+                    <div class="form-group">
+                        <div class="col-sm-6">
+                            <input type="email" class="form-control" id="inputEmail3" placeholder="Email"></input>
+                        </div>
+                    </div>
+                </form> */}
             </div>
         );
     };
