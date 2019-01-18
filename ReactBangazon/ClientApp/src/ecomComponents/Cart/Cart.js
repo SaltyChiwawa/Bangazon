ef2323c2ceb485246a2f63f8c206d99c25674248
Anna Dolan <anna.dolan115@gmail.com>
2019-01-17 20:49:46 -0600

    working on logout, but getting error that runaway is not a function

--- ReactBangazon/ClientApp/src/ecomComponents/Cart/Cart.js
+++ ReactBangazon/ClientApp/src/ecomComponents/Cart/Cart.js
@@ -1,5 +1,5 @@
 ﻿import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 import Nav from '../Navbar/Navbar'
 
--- ReactBangazon/ClientApp/src/ecomComponents/Navbar/Navbar.js
+++ ReactBangazon/ClientApp/src/ecomComponents/Navbar/Navbar.js
@@ -2,48 +2,63 @@
 import logo from '../../Images/bangazon_logo.png';
 import {Link} from 'react-router-dom';
 import './Navbar.css';
-import { Navbar, Button, FormGroup, FormControl} from 'react-bootstrap';
+
+import authRequests from '../../firebaseRequests/auth';
 
 class Nav extends React.Component {
     state = {
         value: '',
+        authed: false,
     };
 
      handleChange(e) {
         this.setState({ value: e.target.value });
-    }
+    };
+
     render() {
+        const {authed, runAway} = this.props;
+        const logoutClickEvent = () => {
+          authRequests.logoutUser();
+          runAway();
+        }
+
         return (
             <div className='Navbar'>
-                <Navbar>
-                    <Navbar.Header>
-                        <Navbar.Brand>
-                            <a class="navbar-brand" href="#home"><img src={logo} responsive id="nav-logo" alt="Bangazon Logo" /></a>
-                        </Navbar.Brand>
-                    </Navbar.Header>
-                        <form class="form-horizontal" id="nav-search">
-                            <div class="form-group">
-                                <div class="col-sm-5 col-sm-offset-1">
-                                    <input 
-                                        type="text" 
-                                        class="form-control" 
-                                        placeholder="Search"
-                                        value={this.state.value}
-                                        onChange={this.handleChange}
-                                    ></input>
-                                </div>
-                                <button type="button" class="btn btn-warning" id="nav-search-btn">
-                                    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-                                </button>
-                                <Link to='/Login' id="nav-login-link">Login</Link>
-                                <span 
-                                    id="nav-cart-logo"
-                                    class="glyphicon glyphicon-shopping-cart pull-right" 
-                                    aria-hidden="true"
-                                >Cart</span>
+                <nav className="navbar">
+                    <div className="navbar-header">
+                        <Link to='/' className="navbar-brand"><img src={logo} responsive id="nav-logo" alt="Bangazon Logo" /></Link>
+                    </div>
+                    <form class="form-horizontal" id="nav-search">
+                        <div class="form-group">
+                            <div class="col-sm-5 col-sm-offset-1">
+                                <input 
+                                    type="text" 
+                                    class="form-control" 
+                                    placeholder="Search"
+                                    value={this.state.value}
+                                    onChange={this.handleChange}
+                                ></input>
                             </div>
-                        </form>
-                </Navbar>
+                            <button type="button" class="btn btn-warning" id="nav-search-btn">
+                                <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                            </button>
+                                {/* {
+                                    authed? (
+                                        <p>Hello | <a onClick={logoutClickEvent}>Logout</a></p>
+                                    ) : (
+                                        <Link to='/Login' class="nav-login-link">Login</Link>
+                                    )
+                                } */}
+                            <p>Hello | <a onClick={logoutClickEvent}>Logout</a></p>
+                            <Link to='/Login' class="nav-login-link">Login</Link>
+                            <span 
+                                id="nav-cart-logo"
+                                class="glyphicon glyphicon-shopping-cart pull-right" 
+                                aria-hidden="true"
+                            >Cart</span>
+                        </div>
+                    </form>
+                </nav>
             </div>
         );
     };
