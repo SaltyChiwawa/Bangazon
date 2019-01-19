5e3534cc91ea8f15ccad1672aac618f1333eec68
Mehret Kulala <merutaye@gmail.com>
2019-01-18 18:28:20 -0600

    cart page wip

--- ReactBangazon/ClientApp/src/ecomComponents/Checkout/Checkout.js
+++ ReactBangazon/ClientApp/src/ecomComponents/Checkout/Checkout.js
@@ -1,55 +1,12 @@
-﻿import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
-
-
+﻿import React from 'react';
 // import { Link } from 'react-router-dom';
 
-class Checkout extends Component {
+class Checkout extends React.Component {
     render() {
         return (
-            <div className='Cart'>
-                    <div>
-                    <h3>Your shopping cart</h3>
-                    <Row>
-                        <Col md={3}>
-                            <button type="button" className="btn btn-success"> Add new product </button>
-                        </Col>
-                        <Col md={3} className="pull-right">
-                            <button type="button" className="btn btn-sucess pull-right">Proceed to checkout</button>
-                        </Col>
-                    </Row>
-                    {/* Navigation buttons */}
-                    <br />
-                    {/* CART PANEL */}
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">
-                                <Row className="vertical-align">
-                                    <Col md={6} className="justify-left">Item(s)</Col>
-                                    <Col md={2} className="justify-center">Price</Col>
-                                    <Col md={2} className="justify-center">Quantity</Col>
-                                    <Col md={2} className="justify-right">SubTotal</Col>
-                                </Row>
-                            </h3>
-                        </div>
-                        <div class="panel-body">
-                            Panel content
-                        </div>
-                        <div class="panel-footer">Panel footer</div>
-                    </div>
-                    {/* CART PANEL */}
-                    {/* Navigation buttons */}
-                    <Row>
-                        <Col md={3}>
-                            <button type="button" className="btn btn-success"> Add new product </button>
-                        </Col>
-                        <Col md={3} className="pull-right">
-                            <button type="button" className="btn btn-sucess pull-right">Proceed to checkout</button>
-                        </Col>
-                    </Row>
-                    {/* Navigation buttons */}
-                </div>
-             </div>
+            <div className='Checkout'>
+
+            </div>
         );
     };
 }
