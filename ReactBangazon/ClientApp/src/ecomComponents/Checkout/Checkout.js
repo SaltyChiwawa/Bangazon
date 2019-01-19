3f571f6c6b0c7dce0c62613b5943f99798dc1ea3
Drake Morrison <drakethedragonprogrammer@gmail.com>
2019-01-19 14:05:32 -0600

    form in progress

--- ReactBangazon/ClientApp/src/ecomComponents/Checkout/Checkout.js
+++ ReactBangazon/ClientApp/src/ecomComponents/Checkout/Checkout.js
@@ -1,57 +1,44 @@
 ﻿import React, { Component } from 'react';
+import { FormGroup, FormControl, FormLabel } from 'react-bootstrap';
 
 import Nav from '../Navbar/Navbar';
-import { Row, Col } from 'react-bootstrap';
 
 class Checkout extends Component {
-    state = {}
+    state = {
+        orderId: '',
+        customerId: '',
+        paymentTypeId: '',
+    }
+
+    getValidationState() {
+        const length = this.state.value.length;
+        if (length > 1) return 'error';
+    }
 
     render() {
 
-        const header = (<Row className="vertical-align">
-            <Col md={6} className="justify-left">Item(s)</Col>
-            <Col md={2} className="justify-center">Price</Col>
-            <Col md={2} className="justify-center">Quantity</Col>
-            <Col md={2} className="justify-right">SubTotal</Col>
-        </Row>);
-
-        const footer = (<Row>
-            <Col md={7}></Col>
-            <Col md={5} className="">
-                <Row className="vertical-align">
-                    <Col md={8} className="justify-right">Total</Col>
-                    <Col md={4} className="green justify-right">
-                        <span>
-                            $10.00
-                                </span>
-                    </Col>
-                </Row>
-            </Col>
-        </Row>);
+        const orderData = '';
 
         return (
             <div className='Checkout'>
-                <div>
-                    <div className="col-xs-12">
-                        <Nav />
-                    </div>
-                    <br />
-                    <h3>Review before you Checkout</h3>
-                    <br />
-                    {/* CART PANEL */}
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">
-                                {header}
-                            </h3>
-                        </div>
-                        <div class="panel-body">
-                            Panel content
-                        </div>
-                        <div class="panel-footer">{footer}</div>
-                    </div>
-                    {/* CART PANEL */}
+                <div className="col-xs-12">
+                    <Nav />
                 </div>
+
+                <h2>Checkout Page</h2>
+
+                <table class='table table-striped table-responsive'>
+                    {orderData}
+                </table>
+
+                <form>
+                    <FormGroup
+                        controlId="paymentTypesForm"
+                        validationState={this.getValidationState()}
+                    >
+
+                    </FormGroup>
+                </form>
             </div>
         );
     };
